b8ec4396105aed4bda4683df8d3373cf12d9b908
DogukanTopallar <dtopallar1@gmail.com>
2022-12-28 15:12:40 +0300

    utils shared

---
+++ utils.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 13 00:00:18 2022
+
+@author: DOGUKAN
+"""
+import pandas as pd #dataset işlemleri
+import numpy as np
+import os #dizin işlemleri & işletim sistemi işlemleri
+import tqdm
+from sklearn.model_selection import train_test_split #test eğitim ayrımı
+from tensorflow.keras.layers import Dense, Dropout #LSTM
+from tensorflow.keras.models import Sequential
+
+
+#etiketleri 1-0 olarak değiştirecek dictionary oluşturuluyor.
+label2int = {
+    "male": 1,
+    "female": 0
+}
+
+def load_data(vector_length=128):
+    if not os.path.isdir(r"C:\Users\Dogukan\Desktop\DERSLER\BM YÜKSEK LİSANS\ÇALIŞMALAR\ANACONDA-SPYDER\GenderClassificationByVoice\Automatic-Gender-Classification-by-Speech\results"):
+        os.mkdir(r"C:\Users\Dogukan\Desktop\DERSLER\BM YÜKSEK LİSANS\ÇALIŞMALAR\ANACONDA-SPYDER\GenderClassificationByVoice\Automatic-Gender-Classification-by-Speech\results")
+        #os.path.isdir -> parametre olarak gönderilen path'in dizin olup olmadığını kontrol ediyor.
+        #os.mkdir -> yeni dizin oluşturmayı sağlıyor.
+        
+    if os.path.isfile(r"C:\Users\Dogukan\Desktop\DERSLER\BM YÜKSEK LİSANS\ÇALIŞMALAR\ANACONDA-SPYDER\GenderClassificationByVoice\Automatic-Gender-Classification-by-Speech\results\features.npy") and os.path.isfile(r"C:\Users\Dogukan\Desktop\DERSLER\BM YÜKSEK LİSANS\ÇALIŞMALAR\ANACONDA-SPYDER\GenderClassificationByVoice\Automatic-Gender-Classification-by-Speech\results\labels.npy"):
+        #os.path.isfile -> verilen yolun gerçek bir dosya yolu olup olmadığını kontrol eder.
+        
+        X = np.load(r"C:\Users\Dogukan\Desktop\DERSLER\BM YÜKSEK LİSANS\ÇALIŞMALAR\ANACONDA-SPYDER\GenderClassificationByVoice\Automatic-Gender-Classification-by-Speech\results\features.npy")
+        y = np.load(r"C:\Users\Dogukan\Desktop\DERSLER\BM YÜKSEK LİSANS\ÇALIŞMALAR\ANACONDA-SPYDER\GenderClassificationByVoice\Automatic-Gender-Classification-by-Speech\results\labels.npy")
+        return X, y
+    
+    df = pd.read_csv(r"C:\Users\Dogukan\Desktop\DERSLER\BM YÜKSEK LİSANS\ÇALIŞMALAR\ANACONDA-SPYDER\GenderClassificationByVoice\Automatic-Gender-Classification-by-Speech\balanced-all.csv")
+
+    
+    n_samples = len(df)
+    # Toplam örnek sayısını getirir
+    
+    n_male_samples = len(df[df['gender'] == 'male'])
+    # Toplam erkek örnek sayısını getirir
+    
+    n_female_samples = len(df[df['gender'] == 'female'])
+    # Toplam kadın örnek sayısını getirir
+    
+    print("Toplam Örnek Sayısı:", n_samples)
+    print("Toplam Erkek Örnek Sayısı:", n_male_samples)
+    print("Toplam Kadın Örnek Sayısı:", n_female_samples)
+    
+    
+    X = np.zeros((n_samples, vector_length))
+    #Belirlenen vektör uzunluğunda tüm ses örnekleri için boş bir dizi oluşturulur.
+    
+    y = np.zeros((n_samples, 1))
+    #Tüm ses etiketleri için boş bir dizi başlatılır.
+    
+    for i, (filename, gender) in tqdm.tqdm(enumerate(zip(df['filename'], df['gender'])), "Loading data", total=n_samples):
+        features = np.load(filename)
+        X[i] = features
+        y[i] = label2int[gender]
+        
+        np.save("results/features", X)
+        np.save("results/labels", y)
+        return X, y
+
+def split_data(X, y, test_size=0.1, valid_size=0.1):
+#%80 train_set, %10 test, %10 valid(model performansını doğrulamak için kullanılır) olarak ayrılır.
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=7)
+    #Ham veri içerisinden %10'u test olacak şekilde train ve test verilerini ayırır.
+    
+    X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=valid_size, random_state=7)
+    #Ayrılan veriler içerisinden %10'u valid(model doğrulamak için) olacak şekilde train ve valid verilerini ayırır.
+    
+    return {
+        "X_train": X_train,
+        "X_valid": X_valid,
+        "X_test": X_test,
+        "y_train": y_train,
+        "y_valid": y_valid,
+        "y_test": y_test
+    }
+    # Bir dictionary olarak ayrılmış verileri saklar.
+
+X, y = load_data()
+data = split_data(X, y, test_size=0.1, valid_size=0.1)
+   
+
+def create_model(vector_length=128):
+    """256 birimden 64'e 5 gizli yoğun katman. """
+    model = Sequential()
+    model.add(Dense(256, input_shape=(vector_length,)))
+    model.add(Dropout(0.3))
+    model.add(Dense(256, activation="relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(128, activation="relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(128, activation="relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(64, activation="relu"))
+    model.add(Dropout(0.3))
+    # one output neuron with sigmoid activation function, 0 means female, 1 means male
+    model.add(Dense(1, activation="sigmoid"))
+    # using binary crossentropy as it's male/female classification (binary)
+    model.compile(loss="binary_crossentropy", metrics=["accuracy"], optimizer="adam")
+    # print summary of the model
+    model.summary()
+    return model
+
+
+
+
+
+
+
