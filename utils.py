9e3bf3e4d39ad3679507cbd5cd6d01c712081cf4
DogukanTopallar <dtopallar1@gmail.com>
2023-04-02 15:17:29 +0300

    trained a new model.

--- utils.py
+++ utils.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 import os
 import tqdm
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM, Dropout
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout
 from sklearn.model_selection import train_test_split
 
 
